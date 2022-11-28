b6959da0d0820ea608d0f6681a1e8b3213a41897
Viktorija K <viktoria.kuluk@inbox.lv>
2022-11-28 20:34:32 +0200

    First commit

---
+++ DataAccess/Repositories/ItemsRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Repositories
+{
+    public class ItemsRepository
+    {
+        public IQueryable<Item> GetItems() 
+        { 
+
+        }
+
+        public void AddItem() 
+        { 
+
+        }
+    }
+}
---
+++ Domain/Models/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Domain.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public double Price { get; set; }
+
+        public string Description { get; set; }
+
+        public string PhotoPath { get; set; }
+
+        public int Stock { get; set; }
+    }
+}
